8cf086370e53e281a64e42d73536ce267b242651
ilmechaJu <pawor006@naver.com>
2024-11-14 11:17:41 +0900

    feat : transfer complete.

---
+++ var.tf
@@ -0,0 +1,12 @@
+variable "AWS_REGION" {
+  default = "ap-northeast-2"
+}
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "APP_NAME" {
+  default = "FitNus"
+}
+
+variable "Environment" {
+  default = "dev"
+}
